64d84ab1e5479e99016aaa257facda74d0c0dbdd
Mark Smearcheck <mark.smearcheck@afit.edu>
2016-08-19 18:05:20 -0500

    Removed listener in contentscript.js becasue it isn't needed for now.

--- js/contentscript.js
+++ js/contentscript.js
@@ -3,16 +3,6 @@
 *
 */
 
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if( request.message === "clicked_browser_action" ) {
-      var firstHref = $("a[href^='http']").eq(0).attr("href");
-
-      console.log(firstHref);
-    }
-  }
-);
-
 var elements = document.getElementsByTagName('*');
 
 for (var i = 0; i < elements.length; i++) {
