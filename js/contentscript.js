f05ff76d309876a6b91a9e6c6dff73bb7cb12007
Mark Smearcheck <mark.smearcheck@afit.edu>
2016-08-23 18:20:08 -0500

    Figured out how to get and set attributes with jquery

--- js/contentscript.js
+++ js/contentscript.js
@@ -5,29 +5,47 @@
 
 var elements = document.getElementsByTagName('*');
 
+
+// Traverse the DOM and search for text
 for (var i = 0; i < elements.length; i++) {
     var element = elements[i];
-
+    console.log(element)
     for (var j = 0; j < element.childNodes.length; j++) {
         var node = element.childNodes[j];
 
         if (node.nodeType === 3) {
             var text = node.nodeValue;
-            var replacedText = text.replace(/Murphy/gi, 'Ass');
+            var replacedText = text.replace(/Murphy/gi, 'God');
 
             if (replacedText !== text) {
-                element.replaceChild(document.createTextNode(replacedText), node);
+                //element.replaceChild(document.createTextNode(replacedText), node);
             }
         }
     }
 }
-printnames();
 
+$(document).ready(function(){
+
+   // jQuery methods go here...
+   printnames();
+
+});
 
 function printnames() {
-    console.log("here")
-    $('.playertablePlayerName').children(':first-child').each(function () {
-        console.log($(this).text());
+    roster = []
+    id = []
+    $('.playertablePlayerName').children([':first-child']).each(function () {
+        //console.log($(this).text());
+        roster.push($(this).text());
+        // Example of changing text
+        // $(this).text("Mark Smearcheck");
     });
+    // Example of getting attribute
+    
+    $('.playertablePlayerName').children([':first-child']).each(function () {
+        //console.log($(this).text());
+        id.push($(this).attr("playerid"));
+    });
+    console.log(roster);
+    console.log(id);
 }
-
