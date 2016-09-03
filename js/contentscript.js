35d326fea0f922373e3492b4ec239c936a3778fc
Mark Smearcheck <mark.smearcheck@afit.edu>
2016-09-02 20:55:37 -0500

    New html columns are created under research and populated with cost and year from the google sheet

--- js/contentscript.js
+++ js/contentscript.js
@@ -6,11 +6,19 @@
 // TODO: Add cut column to roster db.
 // TODO: How do I handle spaces in last names like Jr or Names like Byung
 //       Ho Park?
+// TODO: Only load from Google Sheets once
+
+var observerConfig = {
+    childList: true,
+    characterData: true,
+    subtree: false  // Prevents infinite loop in MutationObserver
+};
 
 $(document).ready(function(){
 
-  importSheet(); // Async event due to .getJSON so need to run
+    importSheet(); // Async event due to .getJSON so need to run
                  // rest of script from with .getJSON.
+    addESPNEvents();
 });
 
 
@@ -36,6 +44,22 @@ function replacetext(){
     }
 }
 
+function addESPNEvents() {
+    /* Reruns importSheet() when a button is clicked on the page. If this is
+       done, then the page does not render when a button is clicked.
+    */
+
+    var target = document.querySelector('.playerTableContainerDiv');
+
+    var observerESPN = new MutationObserver(function (mutations) {
+        observerESPN.disconnect();
+        if (mutations.length > 0) {
+            importSheet();
+        }
+        observerESPN.observe(target, observerConfig);
+    });
+    observerESPN.observe(target, observerConfig);
+}
 
 function importSheet(){
     /* Get the Player Auction Values Google Sheet as JSON. Parse player auction
@@ -74,32 +98,37 @@ function importSheet(){
         console.log(rosterdb)
         // Get the site players and match to rosterdb
         roster = get_fantasy_site_player_names();
+        var site_player_db_info = [];
+
         for (i = 0; i < roster.length; i++) {
-            var site_player_db_info = match_player_site_to_rosterdb(rosterdb, roster[i]);
+            site_player_db_info.push(match_player_site_to_rosterdb(rosterdb, roster[i]));
             console.log(roster[i] + " costs " + site_player_db_info.cost +
                         " and was signed in " + site_player_db_info.year);
         }
+        populate_site_player_table(site_player_db_info);
 
     });
 }
 
-
 function get_fantasy_site_player_names() {
 
-    roster = []
-    id = []
+    roster = [];
+    id = [];
     $('.playertablePlayerName').children([':first-child']).each(function () {
         //console.log($(this).text());
         if ($(this).text() !== "" && $(this).text() !== "PP" &&
             $(this).text() !== "DL15" && $(this).text() !== "DL60" &&
             $(this).text() !== "DL7" && $(this).text() !== "SSPD" &&
-            $(this).text() !== "DTD")
+            $(this).text() !== "DTD" && $(this).text() !== "S")
         {
           roster.push($(this).text());
           // Example of changing text
-          $(this).append(", $1")
+          //$(this).append(", ($1, 2016)")
+          $(this).append("")
         }
+        //$("<a>hi</a>").insertAfter($(this));
     });
+
     // Example of getting attribute
     $('.playertablePlayerName').children([':first-child']).each(function () {
         //console.log($(this).text());
@@ -117,16 +146,14 @@ function match_player_site_to_rosterdb(rosterdb, site_player){
     // TODO: How do I handle spaces in last names like JR or Names like Byung
     //       Ho Park?
     var site_player_db_info = {
-        cost: 0,
-        year: 0
+        cost: '0',
+        year: '--'
     }
-
     index = site_player.indexOf(" ");
     first_name = site_player.substr(0, index);
     last_name = site_player.substr(index+1);
 
     for (j = 0; j < rosterdb.length; j++){
-
         // Match site_player's last name to rosterdb player.
         var site_player_last_name = new RegExp(last_name);
         var result_last = site_player_last_name.test(rosterdb[j].last)
@@ -145,5 +172,28 @@ function match_player_site_to_rosterdb(rosterdb, site_player){
         }
     }
     return site_player_db_info;
+}
+
+function populate_site_player_table(site_player_db_info){
+
+    var header = $('.playerTableTable tr:nth-child(2) td:nth-child(2)').text();
+    $('.playerTableTable tr:nth-child(1) th:last-child').attr('colspan', '5')
+    $('.playerTableTable tr:nth-child(2) td:last-child').after('<td width="25px">COST</td>');
+    $('.playerTableTable tr:nth-child(2) td:last-child').after('<td width="25px">YEAR</td>');
+
+    ii = 0;
+    $('tr.pncPlayerRow td:last-child').each(function(){
+        if ($(this).text() != '--'){
+            $(this).after('<td class="playertableData">$' + String(site_player_db_info[ii].cost) + '</td>');
+            ii = ii + 1;
+        }
+    })
+    ii = 0;
+    $('tr.pncPlayerRow td:last-child').each(function(){
+        if ($(this).text() != '--'){
+            $(this).after('<td class="playertableData">' + String(site_player_db_info[ii].year) + '</td>');
+            ii = ii + 1;
+        }
+    })
 
 }
